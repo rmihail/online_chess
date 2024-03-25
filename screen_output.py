aeec3de5a2a07181fad5bdda6c30fe6cbbce1bf5
rmihail <135147592+rmihail@users.noreply.github.com>
2024-03-26 01:23:34 +0300

    Commented code, resized images, fixed printing figures and moved code to another file

--- screen_output.py
+++ screen_output.py
@@ -1,10 +1,12 @@
 import pygame
 import chess
+from print_functions.print_figures import print_figures
 
 pygame.init()
 
-WIDTH = 700
-HEIGHT = 700
+# ширина, высота, объявление переменной экрана, ограничитель fps и переменная самой доски
+WIDTH = 800
+HEIGHT = 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 timer = pygame.time.Clock()
 fps = 120
@@ -12,6 +14,7 @@ board = chess.Board()
 
 font1 = pygame.font.Font(None, 40)
 
+# прописал координаты для удобства, для примера 19 клетка находится на 2 горизонтали
 coordinates = {(0, 1, 2, 3, 4, 5, 6, 7): 0,
                (8, 9, 10, 11, 12, 13, 14, 15): 1,
                (16, 17, 18, 19, 20, 21, 22, 23): 2,
@@ -21,38 +24,35 @@ coordinates = {(0, 1, 2, 3, 4, 5, 6, 7): 0,
                (48, 49, 50, 51, 52, 53, 54, 55): 6,
                (56, 57, 58, 59, 60, 61, 62, 63): 7}
 
-pieces = {'p': pygame.image.load('images/black_pawn.png').convert(),
-          'n': pygame.image.load('images/black_knight.png').convert(),
-          'b': pygame.image.load('images/black_bishop.png').convert(),
-          'r': pygame.image.load('images/black_rook.png').convert(),
-          'q': pygame.image.load('images/black_queen.png').convert(),
-          'k': pygame.image.load('images/black_king.png').convert(),
-          'P': pygame.image.load('images/white_pawn.png').convert(),
-          'N': pygame.image.load('images/white_knight.png').convert(),
-          'B': pygame.image.load('images/white_bishop.png').convert(),
-          'R': pygame.image.load('images/white_rook.png').convert(),
-          'Q': pygame.image.load('images/white_queen.png').convert(),
-          'K': pygame.image.load('images/white_king.png').convert(),
+# загрузка изображений для использования
+pieces = {'p': pygame.image.load('images/black_pawn.png'),
+          'n': pygame.image.load('images/black_knight.png'),
+          'b': pygame.image.load('images/black_bishop.png'),
+          'r': pygame.image.load('images/black_rook.png'),
+          'q': pygame.image.load('images/black_queen.png'),
+          'k': pygame.image.load('images/black_king.png'),
+          'P': pygame.image.load('images/white_pawn.png'),
+          'N': pygame.image.load('images/white_knight.png'),
+          'B': pygame.image.load('images/white_bishop.png'),
+          'R': pygame.image.load('images/white_rook.png'),
+          'Q': pygame.image.load('images/white_queen.png'),
+          'K': pygame.image.load('images/white_king.png'),
           }
 board_image = pygame.image.load('images/board.png').convert()
+# изменю размер изображений фигур
+for symbol, piece_image in zip(pieces.keys(), pieces.values()):
+    pieces[symbol] = pygame.transform.scale(piece_image, (100, 100))
+
 
 run = True
 while run is True:
     timer.tick(fps)
     screen.fill('dark gray')
 
+    # рисование доски
     screen.blit(board_image, (0, 0))
 
-    f_piece_map = board.piece_map()
-    for square in range(64):
-        piece = board.piece_at(square)
-        if piece is None:
-            pass
-        else:
-            for key in coordinates.keys():
-                if square in key:
-                    horizontal = coordinates[key]
-            screen.blit(pieces[str(piece)], ((square % 8)*100, 700-(square//8)*100))  # тут я напишу свое расположение на доске, сейчас краденное со статьи
+    print_figures(screen, board, coordinates, pieces)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
